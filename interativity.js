d2bfc8441521901009b1b7c73b3fef36b7626815
Matheus Moraes <matheusbmoraes0@gmail.com>
2025-01-19 16:52:34 -0300

    menu para telas inferiores semi-finalizado, falta apenas corrigir um bug de display e suavizar a entrada e saída

--- interativity.js
+++ interativity.js
@@ -1,8 +1,35 @@
 document.addEventListener('DOMContentLoaded', (charge_complete) => {
+
     const form = document.getElementById('Form');
+    const contact = document.getElementById('contact');
+    const menu_logo = document.getElementById('menu-logo');
+
+    const display_contact = () => {
+        if (window.innerWidth <=768) {
+            contact.style.display = 'none';
+        } else {
+            contact.style.display = 'block';
+        }
+    };
+
+    display_contact();
+    window.addEventListener('resize', display_contact);
+
     form.addEventListener('submit', (submit_cancel) => {
         submit_cancel.preventDefault();
         alert('Mensagem enviada com sucesso!');
         window.location.reload();
     });
+    
+    menu_logo.addEventListener('mouseover', (over_menu) => {
+        menu_logo.style.transform = 'rotate(90deg)';
+        contact.style.display = 'block';
+    });
+    
+    contact.addEventListener('mouseout', (out_menu) => {
+        menu_logo.style.transform = 'rotate(0deg)';
+        contact.style.display = 'none';
+    });
+
+    
 });
